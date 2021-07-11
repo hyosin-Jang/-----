affb1e622a92fdc9db2efe31af613fc789f428f8
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-07-11 19:33:35 +0900

    Add files via upload

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import {Typography, Button, Form, Input} from 'antd';
+//const { Title } = Typography;
+const { TextArea } = Input;
+
+function UploadProductPage() {
+
+    const [Title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)
+    const [Continent, setContinent] = useState(1)
+    const [Images, setImages] = useState([])
+
+    // 이미지를 가져오기 위한 state
+    // titleChangeHandler 함수를 만들어준다.
+    const titleChangeHandler = (event) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const descriptionChangeHandler = (event) => {
+        setDescription(event.currentTarget.value)
+    }
+
+    const priceChangeHandler = (event) => {
+        setPrice(event.currentTarget.value)
+    }
+
+    return (
+        <div style = {{ maxWidth:'700px', margin:'2rem auto' }}>
+            <div style= {{ textAlign:'center', marginBottom:'2rem'}}>
+                <h2>여행 상품 업로드</h2>
+            </div>
+
+            <Form>
+                {/* Dropzone */}
+                <br />
+                <br />
+                <label>이름</label>
+                <Input onChange={titleChangeHandler} value={Title}/>
+                <br />
+                <br />
+                <label>설명</label>
+                <TextArea onChange={descriptionChangeHandler} value={Description}/>
+                <br />
+                <label>가격($)</label>
+                <Input type = "number" onChange={priceChangeHandler} value={Price}/>
+                <br />
+                <br />
+                <select>
+                    <option></option>
+                </select>
+                <br />
+                <br />
+                <Button>
+                    확인
+                </Button>
+            </Form>
+
+
+        </div>
+    )
+}
+
+export default UploadProductPage
