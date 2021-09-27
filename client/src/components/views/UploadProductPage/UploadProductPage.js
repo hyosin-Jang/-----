a62a70ce8422b69bd17a61af27ce778c43c576c0
ssini <cdnnnl@ewhain.net>
2021-09-28 00:01:01 +0900

    Update select option

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 import {Typography, Button, Form, Input} from 'antd';
-//const { Title } = Typography;
+const { Title } = Typography;
 const { TextArea } = Input;
+
+const Continents = [
+    {key:1, value:"Africa"},
+    {key:2, value: "Europe"},
+    {key:3, value: "Asia"},
+    {key:4, value: "North America"},
+    {key:5, value: "South America"},
+    {key:6, value: "Australia"},
+    {key:7, value: "Antarctica"},
+]
 
 function UploadProductPage() {
 
-    const [Title, setTitle] = useState("")
+    const [title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
     const [Price, setPrice] = useState(0)
     const [Continent, setContinent] = useState(1)
     const [Images, setImages] = useState([])
 
-    // 이미지를 가져오기 위한 state
-    // titleChangeHandler 함수를 만들어준다.
     const titleChangeHandler = (event) => {
         setTitle(event.currentTarget.value)
     }
@@ -25,10 +33,14 @@ function UploadProductPage() {
         setPrice(event.currentTarget.value)
     }
 
+    const continentChangeHandler = (event) => {
+        setContinent(event.currentTarget.value)
+    }
+
     return (
         <div style = {{ maxWidth:'700px', margin:'2rem auto' }}>
             <div style= {{ textAlign:'center', marginBottom:'2rem'}}>
-                <h2>여행 상품 업로드</h2>
+                <Title level={2}>여행 상품 업로드</Title>
             </div>
 
             <Form>
@@ -36,7 +48,7 @@ function UploadProductPage() {
                 <br />
                 <br />
                 <label>이름</label>
-                <Input onChange={titleChangeHandler} value={Title}/>
+                <Input onChange={titleChangeHandler} value={title}/>
                 <br />
                 <br />
                 <label>설명</label>
@@ -46,8 +58,11 @@ function UploadProductPage() {
                 <Input type = "number" onChange={priceChangeHandler} value={Price}/>
                 <br />
                 <br />
-                <select>
-                    <option></option>
+                <select onChange={continentChangeHandler} value={Continent}>
+                    {Continents.map(item => (
+                        <option key = {item.key} vlaue={item.key}>{item.value}</option>
+                    ))}
+                    
                 </select>
                 <br />
                 <br />
